8b880eb9585a391aee3af113ed2c015a068d3586
SerapG <serapgulgen1@gmail.com>
2024-10-24 15:44:09 +0300

    destructing

---
+++ destructing.js
@@ -0,0 +1,17 @@
+/*
+const degerler = {
+  deger1: "deger1",
+  deger2: "deger2",
+  deger3: "deger3",
+};
+
+//const deger1 = degerler.deger1;
+//const deger2 = degerler.deger2;
+
+const { deger1, deger2, deger3, deger4 = "deger4" } = degerler;
+console.log(deger1, deger2, deger3, deger4);
+*/
+
+const degerler = [1, 2, 3];
+const [deger1, deger2, deger3] = degerler;
+console.log(deger1, deger2, deger3);
